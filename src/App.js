3ae0afb7b4fba93654089b097926454c5c7bd399
SuperDork <bearbaier@gmail.com>
2023-02-06 23:23:35 -0600

    added navbar css and basic jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
